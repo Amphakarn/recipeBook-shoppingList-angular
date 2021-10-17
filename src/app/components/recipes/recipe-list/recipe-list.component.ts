39caaeb18ff9d5f1bd9fe6cb0af29077e4f79ed1
Amphakarn <pisuthigomol@yahoo.com>
2021-10-17 15:11:33 -0400

    creating recipe-list component

--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,15 @@ import { Component } from "@angular/core";
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe Name', 'This is a test recipe description', 'https://www.modernfarmhouseeats.com/wp-content/uploads/2021/03/chili-lime-shrimp-ramen-2-scaled.jpg'),
+  ];
+
   constructor() {
 
   }
+
+  // addRecipe() {
+  //   this.recipes.push('Recipe Name2', 'Description2', 'https://www.modernfarmhouseeats.com/wp-content/uploads/2021/03/chili-lime-shrimp-ramen-2-scaled.jpg')
+  // }
 }
